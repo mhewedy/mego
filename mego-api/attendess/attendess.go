5c4949507da7a8e9c4189da91f5f24e03993fb31
Muhammad Hewedy <mhewedy@gmail.com>
2019-12-06 15:06:43 +0300

    implement Attendees Search api

--- mego-api/attendess/attendess.go
+++ mego-api/attendess/attendess.go
@@ -12,11 +12,13 @@ var attendOnce sync.Once
 
 func ListAttendees(w http.ResponseWriter, r *http.Request) {
 	attendOnce.Do(indexAttendees)
-	_ = json.NewEncoder(w).Encode(attendeesIndex)
+
+	json.NewEncoder(w).Encode(attendeesIndex)
 }
 
 func SearchAttendees(w http.ResponseWriter, r *http.Request) {
 	attendOnce.Do(indexAttendees)
 
-	_, _ = w.Write([]byte("SUCCESS"))
+	attendees := searchAttendees(r.URL.Query().Get("q"))
+	json.NewEncoder(w).Encode(attendees)
 }
