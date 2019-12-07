69b60f617a05c811138e8fd7ff28a25b85fba46f
Muhammad Hewedy <mhewedy@gmail.com>
2019-12-07 15:40:49 +0300

    polish rooms module

--- mego-api/rooms/rooms.go
+++ mego-api/rooms/rooms.go
@@ -3,9 +3,11 @@ package rooms
 import (
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 )
 
@@ -51,6 +53,13 @@ func loadRoomList() {
 		log.Fatal(err)
 	}
 
+	// trim spaces
+	for i := range roomList {
+		for j := range roomList[i] {
+			roomList[i][j] = strings.TrimSpace(roomList[i][j])
+		}
+	}
+
 	buildRoomTree()
 }
 
@@ -63,7 +72,6 @@ func buildRoomTree() {
 			continue // skip header
 		}
 		for j, field := range row {
-			//field := strings.TrimSpace(field)
 			if j == building {
 				if !contains(root.Root, field) {
 					root.Root = append(root.Root, Node{
@@ -74,7 +82,7 @@ func buildRoomTree() {
 			}
 			if j == zone {
 				buildingKey := row[j-1]
-				b := find(root.Root, buildingKey)
+				b, _ := find(root.Root, buildingKey)
 				if !contains(b.Children, field) {
 					b.Children = append(b.Children, Node{
 						Key:   field,
@@ -84,9 +92,9 @@ func buildRoomTree() {
 			}
 			if j == size {
 				buildingKey := row[j-2]
-				b := find(root.Root, buildingKey)
+				b, _ := find(root.Root, buildingKey)
 				zoneKey := row[j-1]
-				z := find(b.Children, zoneKey)
+				z, _ := find(b.Children, zoneKey)
 				if !contains(z.Children, field) {
 					z.Children = append(z.Children, Node{
 						Key:   field,
@@ -96,11 +104,11 @@ func buildRoomTree() {
 			}
 			if j == displayName {
 				buildingKey := row[j-3]
-				b := find(root.Root, buildingKey)
+				b, _ := find(root.Root, buildingKey)
 				zoneKey := row[j-2]
-				z := find(b.Children, zoneKey)
+				z, _ := find(b.Children, zoneKey)
 				sizeKey := row[j-1]
-				s := find(z.Children, sizeKey)
+				s, _ := find(z.Children, sizeKey)
 				key := row[code]
 				if !contains(s.Children, key) {
 					s.Children = append(s.Children, Node{
@@ -115,14 +123,19 @@ func buildRoomTree() {
 }
 
 func contains(nodes []Node, key string) bool {
-	return find(nodes, key) != nil
-}
-
-func find(nodes []Node, key string) *Node {
 	for i := range nodes {
 		if nodes[i].Key == key {
-			return &nodes[i]
+			return true
 		}
 	}
-	return nil
+	return false
+}
+
+func find(nodes []Node, key string) (*Node, error) {
+	for i := range nodes {
+		if nodes[i].Key == key {
+			return &nodes[i], nil
+		}
+	}
+	return nil, fmt.Errorf("should never happen: key %s not found in nodes %s", key, nodes)
 }
