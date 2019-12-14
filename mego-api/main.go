baa4d87747466ca4a320f1c1d9c7f3cc87bc4acb
Muhammad Hewedy <mhewedy@gmail.com>
2019-12-14 05:28:39 +0300

    try automatically open browser when server starts

--- mego-api/main.go
+++ mego-api/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/mhewedy/mego/events"
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
 )
 
 var ewsClient ews.Client
@@ -31,6 +33,23 @@ func main() {
 	events.EWSClient = ewsClient
 	attendess.EWSClient = ewsClient
 
-	fmt.Println("Server start listening on port 3000")
-	log.Fatal(http.ListenAndServe(":3000", api.Route()))
+	go func() {
+		fmt.Println("Server start listening on port 3000")
+		log.Fatal(http.ListenAndServe(":3000", api.Route()))
+	}()
+
+	openBrowser("http://localhost:3000")
+
+	select {}
+}
+
+func openBrowser(url string) {
+	switch runtime.GOOS {
+	case "linux":
+		_ = exec.Command("xdg-open", url).Start()
+	case "windows":
+		_ = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		_ = exec.Command("open", url).Start()
+	}
 }
