e957e5777d256cdc76e269eb431cb49583f13aeb
Muhammad Hewedy <mhewedy@gmail.com>
2019-12-15 03:33:41 +0300

    allow requests to all resources but the /api/ as well the login page

--- mego-api/api/middleware.go
+++ mego-api/api/middleware.go
@@ -14,7 +14,7 @@ func AuthMiddleware() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-			if r.RequestURI == "/api/v1/login" {
+			if r.RequestURI == "/api/v1/login" || !strings.HasPrefix(r.RequestURI, "/api/") {
 				next.ServeHTTP(w, r)
 				return
 			}
