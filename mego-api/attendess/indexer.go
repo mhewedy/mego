069c68dffd5bc67f27c98c3378bd072d3f0c2e13
Muhammad Hewedy <mhewedy@gmail.com>
2019-12-18 23:46:00 +0300

    filter matches with occurrence > 1

--- mego-api/attendess/indexer.go
+++ mego-api/attendess/indexer.go
@@ -61,7 +61,7 @@ func sortByOccurrence(temp [][]Attendee) []Attendee {
 
 	result := make([]Attendee, 0)
 	for _, s := range kvs {
-		if s.Value > 1 {
+		if s.Value > conf.GetInt("indexer.min_occurrence", 1) {
 			result = append(result, s.Key)
 		}
 	}
