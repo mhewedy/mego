8ef5b8f70e0946fadc302211959c0d7d658e2a29
Muhammad Hewedy <mhewedy@gmail.com>
2020-02-06 09:09:59 +0300

    create event: send to meeting room display name instead of email to avoid email being unreadable by target invitees

--- mego-api/events/create.go
+++ mego-api/events/create.go
@@ -3,7 +3,9 @@ package events
 import (
 	"github.com/mhewedy/ews/ewsutil"
 	"github.com/mhewedy/mego/commons"
+	"github.com/mhewedy/mego/rooms"
 	"github.com/mhewedy/mego/user"
+	"log"
 	"time"
 )
 
@@ -15,12 +17,18 @@ func doCreate(i *createInput, u *user.User) error {
 		Sent by <a style="color: gray; text-decoration: none;" href="https://github.com/mhewedy/mego" 
 		target="_blank"><span style="font-weight: bold;">MEGO</span></a> The Meeting Organizer</div>`
 
+	name, err := rooms.FindByEmail(i.Room)
+	if err != nil {
+		log.Println(err)
+		name = i.Room
+	}
+
 	return ewsutil.CreateHTMLEvent(ewsClient,
 		i.To,
 		i.Optional,
 		i.Subject,
 		body,
-		i.Room,
+		name,
 		i.From,
 		time.Duration(i.Duration)*time.Minute,
 	)
